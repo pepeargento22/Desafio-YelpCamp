1fdd16c409b63263c94ec84ef51a700724fecaad
Francisco <paco19956@hotmail.com>
2023-01-26 16:15:00 -0300

    agregue usuarios a la lista y comencé a trabajar en la validación del login

--- js/validacionLogin.js
+++ js/validacionLogin.js
@@ -1,11 +1,30 @@
 let formulario = document.querySelector('form');
-let inputs = document.querySelectorAll('input');
-let error = document.querySelector('.error-validacion');
+let input_usuario = document.getElementById('usuario');
+let input_contraseña = document.getElementById('contraseña');
+let error = document.querySelectorAll('.error-validacion');
+
+let login_usuario = {
+    "Username": "",
+    "Password": ""
+}
 
 /* LOS USUARIOS REGISTRADOS DEBEN ESTAR EN UN JSON (TAL VEZ?) DE LA CUAL SE PODRIA CHEQUEAR QUE COINCIDAN EL USUARIO Y CONTRASEÑA.
     EN CASO DE QUE NO COINCIDA CON ALGUNO DE LOS 2 DATOS SE TIRA UN ERROR SIN ACLARAR DE CUAL PROVIENE EL ERROR */
-/*
+
+function verificarUsuario(datos_usuario) {
+    /* por ahi me tire error si hago "datos_usuario.Usuario" y "datos_usuario.Contraseña" */
+    /* cargo la lista de usuarios y chequeo que alguno coincida con los datos del login */
+    return fetch("js/listaUsuarios.json")
+    .then(response => response.json())
+    .then(lista => {
+    console.log(lista);
+    })
+}
+
 formulario.addEventListener('submit', function(e) {
-    e.preventDefault;
+    e.preventDefault();
+    login_usuario.Username = input_usuario.value;
+    login_usuario.Password = input_contraseña.value;
+    console.log(login_usuario);
+    verificarUsuario(login_usuario);
 })
-*/
