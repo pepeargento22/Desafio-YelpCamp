4110a35a2627a5429642c8be91d5ad23dc905b5e
Francisco Garcia Huidobro <paco19956@hotmail.com>
2023-01-26 23:23:12 -0300

    agregada la funcion verificarUsuario() al archivos validacionLogin.js

--- js/validacionLogin.js
+++ js/validacionLogin.js
@@ -9,7 +9,12 @@ let login_usuario = {
 }
 
 /* LOS USUARIOS REGISTRADOS DEBEN ESTAR EN UN JSON (TAL VEZ?) DE LA CUAL SE PODRIA CHEQUEAR QUE COINCIDAN EL USUARIO Y CONTRASEÑA.
-    EN CASO DE QUE NO COINCIDA CON ALGUNO DE LOS 2 DATOS SE TIRA UN ERROR SIN ACLARAR DE CUAL PROVIENE EL ERROR */
+    EN CASO DE QUE NO COINCIDA CON ALGUNO DE LOS 2 DATOS SE TIRA UN ERROR */
+
+function loginYelpCamp() {
+    /* FUNCION DE LOGIN, VOY A TENER QUE LABURAR CON EL LOCAL STORAGE Y TMB EL HISTORY PARA QUE ME MANDE A LA PAGINA DESDE LA CUAL
+    QUISE HACER LOGIN */
+}
 
 function verificarUsuario(datos_usuario) {
     /* por ahi me tire error si hago "datos_usuario.Usuario" y "datos_usuario.Contraseña" */
@@ -17,7 +22,41 @@ function verificarUsuario(datos_usuario) {
     return fetch("js/listaUsuarios.json")
     .then(response => response.json())
     .then(lista => {
-    console.log(lista);
+        console.log(lista);
+        for (i = 0; i < lista.length; i++) {
+            /* chequeo que los usuarios coincidan */
+            if (datos_usuario.Username == lista[i].Username) {
+                /* hago invisible el error de validación en caso de que esté visible */
+                if (error[0].classList.value != 'error-validacion') {
+                    error[0].classList.toggle('error-validacion');
+                    input_usuario.style.border = '0';
+                }
+                /* al coincidir los usuarios, chequeo que las contraseñas coincidan */
+                if (datos_usuario.Password == lista[i].Password) {
+                    /* hago invisible el error de validación en caso de que esté visible */
+                    if (error[1].classList.value != 'error-validacion') {
+                        error[1].classList.toggle('error-validacion');
+                        input_contraseña.style.border = '0';
+                    }
+                    console.log("HAY UN MATCH!");
+                    /* function loginYelpCamp() */
+                    break
+                } else {
+                    /* si la contraseña no coincide, hago visible el error de validación */
+                    if (error[1].classList.value == 'error-validacion') {
+                        error[1].classList.toggle('error-validacion');
+                        input_contraseña.style.border = '2px solid red';
+                    }
+                    break    
+                }
+            } else {
+                /* si ningun usuario coincide, al llegar al último, hago visible el error de validación */
+                if (i == lista.length-1 && error[0].classList.value == 'error-validacion') {
+                    error[0].classList.toggle('error-validacion');
+                }
+            }        
+            
+        };
     })
 }
 
