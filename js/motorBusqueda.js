c288976fc93bf00a25e32a2ccfc68517b8e30406
Francisco Garcia Huidobro <paco19956@hotmail.com>
2023-03-03 01:17:28 -0300

    arrancando con el buscador

--- js/motorBusqueda.js
+++ js/motorBusqueda.js
@@ -1,5 +1,13 @@
 let imagenes = document.querySelectorAll('div.contenedor-imagen img');
 let descripciones = document.querySelectorAll('div.elemento-campground p');
+let nombres_cg = document.querySelectorAll('div.elemento-campground h3');
+const lista_cg = document.createElement('ul'); /* FALTA CREAR LOS LI TMB Y QUE TODO SE LE APLIQUE EL APPEND */
+let contenedor = document.querySelector('.contenedor-busqueda');
+let formulario = document.querySelector('form');
+
+/*  LA IDEA SERIA TENER TODOS LOS NOMBRES DE LOS CAMPGROUNDS Y CARGARLOS A UNA LISTA. DE AHI CON EL EVENTO KEYUP 
+O ALGO ASI (EL QUE SE ACTIVA AL SOLTAR UNA TECLA) EN EL BUSCADOR TE SALTA EL O LOS ITEMS DE LA LISTA QUE MATCHEAN 
+CON LO QUE ESCRIBIS. AL DARLE SUBMIT, TE TIRA EL ELEMENTO CON EL QUE MATCHEA EN LA PARTE DE ABAJO O TE TIRA QUE NO ENCONTRO NADA  */
 
 function calcularAlturaMax(elementos) {
     let h_max = 0;
@@ -20,6 +28,7 @@ function calcularPadding(elems) {
     }
 }
 
+/* con esto re-ajusto diferencias en altura que se puedan dar entre elementos de campgrounds por su texto y el ancho */
 document.addEventListener('DOMContentLoaded', function() {
     if (window.innerWidth > 500) {
         calcularPadding(imagenes);
