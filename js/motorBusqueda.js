307231faa1a18932039ac21bb451f577568963be
Francisco <paco19956@hotmail.com>
2023-03-14 16:07:09 -0300

    elaborando el js para el buscador

--- js/motorBusqueda.js
+++ js/motorBusqueda.js
@@ -2,8 +2,18 @@ let imagenes = document.querySelectorAll('div.contenedor-imagen img');
 let descripciones = document.querySelectorAll('div.elemento-campground p');
 let nombres_cg = document.querySelectorAll('div.elemento-campground h3');
 const lista_cg = document.createElement('ul'); /* FALTA CREAR LOS LI TMB Y QUE TODO SE LE APLIQUE EL APPEND */
-let contenedor = document.querySelector('.contenedor-busqueda');
-let formulario = document.querySelector('form');
+const elems_cg = [];
+let contenedor_cg = document.querySelector('.contenedor-busqueda');
+let input_barra = document.querySelector('.barra-busqueda');
+
+/* Agrego la lista de los campgrounds al documento */
+contenedor_cg.appendChild(lista_cg);
+for (i=0; i < nombres_cg.length; i++) {
+    elems_cg[i] = document.createElement('li');
+    elems_cg[i].textContent = nombres_cg[i].textContent;
+    lista_cg.appendChild(elems_cg[i]);
+}
+
 
 /*  LA IDEA SERIA TENER TODOS LOS NOMBRES DE LOS CAMPGROUNDS Y CARGARLOS A UNA LISTA. DE AHI CON EL EVENTO KEYUP 
 O ALGO ASI (EL QUE SE ACTIVA AL SOLTAR UNA TECLA) EN EL BUSCADOR TE SALTA EL O LOS ITEMS DE LA LISTA QUE MATCHEAN 
@@ -35,3 +45,17 @@ document.addEventListener('DOMContentLoaded', function() {
         calcularPadding(descripciones);
     }
 });
+
+/* SE ME CAYO A PEDAZOS EL CODIGO ACA PERO LA IDEA ESTA */
+input_barra.addEventListener('keyup', function() {
+    console.log('levante el dedito jeje');
+    let texto = input_barra.value;
+    elems_cg.forEach(li => {
+        if(li.style.display == 'block' && li.textContent.contains(texto) == false) {
+            li.style.display = 'none';
+        }
+        if(li.textContent.contains(texto)) {
+            li.style.display = 'block';
+        }
+    })
+})
